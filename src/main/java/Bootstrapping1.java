ab07f8b9de6a671e31449e792be98ec0bd54a86e
eddie1031 <archaco123@gmail.com>
2026-04-22 17:07:49 +0900

    bootstrapping1

---
+++ src/main/java/Bootstrapping1.java
@@ -0,0 +1,15 @@
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Bootstrapping1 {
+
+    void main() {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("hibernate-exp-1");
+
+
+
+    }
+
+}
