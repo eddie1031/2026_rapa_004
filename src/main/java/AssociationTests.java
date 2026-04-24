d78d6a17eb5aa180d28eda1c3cf8a932d4fe449e
eddie1031 <archaco123@gmail.com>
2026-04-24 10:24:25 +0900

    association test1

---
+++ src/main/java/AssociationTests.java
@@ -0,0 +1,69 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import model.s03.Member;
+import model.s03.Order;
+
+public class AssociationTests {
+
+    private static final EntityManagerFactory emf
+            = Persistence.createEntityManagerFactory("hibernate-exp-6");
+
+    void main() {
+        many_to_one_test();
+    }
+
+    void many_to_one_test() {
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        Member member;
+
+        try {
+
+            tx.begin();
+
+            member = new Member("노시한", "hwa", "chicken@hwa.com");
+
+            em.persist(member);
+
+            tx.commit();
+
+        } catch ( Exception e ) {
+            tx.rollback();
+            throw new RuntimeException("테스트에 실패했습니다", e);
+        } finally {
+            em.close();
+        }
+
+        EntityManager em2 = emf.createEntityManager();
+        EntityTransaction tx2 = em2.getTransaction();
+
+        Long orderId;
+
+        try {
+
+            tx2.begin();
+
+            Order order = new Order(
+                    em2.find(Member.class, member.getId())
+            );
+
+            em2.persist(order);
+            tx2.commit();
+            orderId = order.getId();
+
+        } catch ( Exception e ) {
+            tx2.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em2.close();
+        }
+
+
+
+    }
+
+}
